2b258ac91f568e13b45e3df0830c08658a852d09
IgTol <93005010+IgTol@users.noreply.github.com>
2022-01-11 17:28:05 +0300

    3-rd Commit

---
+++ src/main/java/by/itoverone/StorageSystem_18/EasyStorageSystem.java
@@ -0,0 +1,13 @@
+package by.itoverone.StorageSystem_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStorageSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyStorageSystem.class, args);
+	}
+
+}
---
+++ src/test/java/by/itoverone/StorageSystem_18/EasyStorageSystemTests.java
@@ -0,0 +1,13 @@
+package by.itoverone.StorageSystem_18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStorageSystemTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
